a2ecdf760eea2fd4a3e187fc1649c1c2bc6ec4bb
Daniel Socorro <daniel.socorro.c@gmail.com>
2022-10-07 15:49:21 -0300

    conditionals

---
+++ conditionals.py
@@ -0,0 +1,15 @@
+# age = int(input("write your age:"))
+# if age > 17:
+#     print("You are an adult")
+# else:
+#     pass
+#     print("You are younger")
+
+number = int(input('write a number: '))
+
+if number > 5:
+    print('Is bigger than 5')
+elif number == 5:
+    print('Is equal to 5')
+else:
+    print('Is less than 5')
