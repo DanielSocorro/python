4a3e7fdb2134f22f975c80e03cea1daf1d59d9fd
Daniel Socorro <daniel.socorro.c@gmail.com>
2022-10-07 10:46:16 -0300

    arithmics

--- arithmics.py
+++ arithmics.py
@@ -1,1 +1,49 @@
-global
+""" python3 """
+""" Python 3.8.9 (default, Jul 19 2021, 09:37:30)
+[Clang 13.0.0 (clang-1300.0.27.3)] on darwin
+Type "help", "copyright", "credits" or "license" for more information. """
+
+5 + 5
+10
+
+print(5 + 5)
+10
+
+6-3
+3
+
+3+4
+7
+
+5+6
+11
+
+6*6
+36
+
+2: 20
+#File "<stdin>", line 1
+#SyntaxError: illegal target for annotation
+
+
+
+5*5*4*3*2*4 
+2400
+
+21 / 5 
+4.2
+
+21 // 5    # integer division
+4
+
+21 % 5     # remainder of division
+1
+
+2 ** 2    #  power to square
+4
+
+2 ** 5
+32
+
+5 + 5 * 2  # logical operations( last sum operations)
+15
