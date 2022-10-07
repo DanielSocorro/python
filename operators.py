52c00be92f08aea1fd05d8105751c4a0bda5581f
Daniel Socorro <daniel.socorro.c@gmail.com>
2022-10-07 19:57:45 -0300

    chain characters and methods

--- operators.py
+++ operators.py
@@ -44,3 +44,80 @@ False
 
 number1 >= number2
 True
+
+        # ---------------------------------------------------------------------------- #
+
+
+name = input("¿What is your name?: ")
+# ¿What is your name?: Dan
+name
+'Dan '
+
+name.upper()
+'DAN '
+
+name.capitalize()
+'Dan '
+
+name = name.capitalize()
+'Dan '
+
+name = name.strip()
+'Dan'
+
+name = name.lower()
+'dan'
+
+name = name.replace('a', 'o')
+'don'
+
+name[0]
+'d'
+
+name[1]
+'a'
+
+name[2]
+'n'
+
+len(name)
+3
+
+# ------------------------------------------------------------------
+
+name = "Christensen"
+name[0]
+'C'
+
+name[1]
+'h'
+
+name[2]
+'r'
+
+name[0:4]
+'Chri'
+
+name[:4]
+'Chri'
+
+name[4:]
+'stensen'
+
+name[1:6]
+'hrist'
+
+name[1:9]
+'hristens'
+
+name[1:6:2]    # one to six two for two
+'hit'
+
+name[::]       # zero to the end one in one
+'Christensen'
+
+name[1::3]      # one to the end tree in tree
+'hsnn'
+
+name[::-1]      # from the end one by one less
+'nesnetsirhC'
