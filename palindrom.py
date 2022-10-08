6e4b426f7a91c2ff5a4ddcc59a7c39db1174d7ae
Daniel Socorro <daniel.socorro.c@gmail.com>
2022-10-08 18:08:46 -0300

    palindrom app

---
+++ palindrom.py
@@ -0,0 +1,23 @@
+def palindrom(word):
+    word = word.replace(' ', '')
+    word = word.lower()
+    rev_word = word[::-1]
+    if word == rev_word:
+        return True
+    else:
+        return False
+
+def run():
+    word = input('Write a word: ')
+    is_palindrom = palindrom(word)
+    if is_palindrom == True:
+        print('Is Palindrom! 🥳 🎉 🎊 🎁 🪅 🍾')
+    else:
+         print('Is not a Palindrom 😟 ')
+
+
+
+
+
+if __name__ == '__main__':
+    run()
