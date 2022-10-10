420d9a817a9c38c2039fe6a22612be763a3e3ff7
Daniel Socorro <daniel.socorro.c@gmail.com>
2022-10-10 14:05:22 -0300

    cicles in for

---
+++ for.py
@@ -0,0 +1,31 @@
+ 
+print(1)
+print(2)
+print(3)
+print(4)
+print(5) # to 10000
+
+
+# 3 ways to do it !!
+
+counter = 1
+print(counter)
+while counter < 1000:
+    #counter = counter + 1
+    counter +=1
+    print(counter) 
+
+
+a = list(range(1, 1001))
+print(a)
+
+
+for counter in range(1, 1001):
+    print(counter) 
+
+
+# and bonus !
+
+for i in range(10):
+    print(11*i)
+
