b294d3bbb38e24d35556d96bc8db18e9eb3ed8b0
Daniel Socorro <daniel.socorro.c@gmail.com>
2022-10-10 17:44:13 -0300

    break-continue sentence and prime app

---
+++ break-cont.py
@@ -0,0 +1,19 @@
+def run():
+  #  for counter in range(1000):
+   #     if counter % 2 != 0:
+    #        continue
+     #   print(counter)
+
+   # for i in range(10000):
+    #    print(i)
+     #   if i == 8797:
+      #      break
+
+    text = input('write a text: ')
+    for letter in text:
+        if letter == 'o':
+            break
+        print(letter)
+
+if __name__ == '__main__':
+    run()
